01cf845b42b3a47dc83248ea648c28664797d056
Matt Gaunt <mattgaunt@google.com>
2018-09-25 21:30:48 -0700

    Tweak to output of render head assets

--- src/helpers/render-head-assets.ts
+++ src/helpers/render-head-assets.ts
@@ -5,7 +5,7 @@ import {Bundle} from '../create-bundle';
 export function renderHeadAssets(bundle: Bundle) {
   const lines = [];
   for (const inlineStyle of bundle.styles.inline.values()) {
-    lines.push(`<style>${handlebars.escapeExpression(inlineStyle.trim())}</style>`);
+    lines.push(`<style>${inlineStyle.trim()}</style>`);
   }
   for (const syncStyle of bundle.styles.sync.values()) {
     lines.push(`<link rel="stylesheet" type="text/css" href="${handlebars.escapeExpression(syncStyle)}" />`);
